1bd1f49dbab2adf3d0199cade457ac02e1e7c9ec
Bradley <Bradley21890543@users.noreply.github.com>
2017-01-17 16:44:25 -0500

    :[

---
+++ jetson/grip/CoolCode.cpp
@@ -0,0 +1,170 @@
+#include "CoolCode.h"
+/**
+* Initializes a CoolCode.
+*/
+
+namespace grip {
+
+CoolCode::CoolCode() {
+}
+/**
+* Runs an iteration of the Pipeline and updates outputs.
+*
+* Sources need to be set before calling this method. 
+*
+*/
+void CoolCode::process(cv::Mat source0){
+	//Step Resize_Image0:
+	//input
+	cv::Mat resizeImageInput = source0;
+	double resizeImageWidth = 320.0;  // default Double
+	double resizeImageHeight = 240.0;  // default Double
+	int resizeImageInterpolation = cv::INTER_CUBIC;
+	resizeImage(resizeImageInput, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, this->resizeImageOutput);
+	//Step HSV_Threshold0:
+	//input
+	cv::Mat hsvThresholdInput = resizeImageOutput;
+	double hsvThresholdHue[] = {41.62385249309404, 92.23793941146276};
+	double hsvThresholdSaturation[] = {0.0, 255.0};
+	double hsvThresholdValue[] = {222.76464527030643, 255.0};
+	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = hsvThresholdOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+	//Step Filter_Contours0:
+	//input
+	std::vector<std::vector<cv::Point> > filterContoursContours = findContoursOutput;
+	double filterContoursMinArea = 25.0;  // default Double
+	double filterContoursMinPerimeter = 25.0;  // default Double
+	double filterContoursMinWidth = 10.0;  // default Double
+	double filterContoursMaxWidth = 1000.0;  // default Double
+	double filterContoursMinHeight = 0.0;  // default Double
+	double filterContoursMaxHeight = 1000.0;  // default Double
+	double filterContoursSolidity[] = {51.927029314658625, 100.0};
+	double filterContoursMaxVertices = 200.0;  // default Double
+	double filterContoursMinVertices = 0.0;  // default Double
+	double filterContoursMinRatio = 0.0;  // default Double
+	double filterContoursMaxRatio = 100.0;  // default Double
+	filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, this->filterContoursOutput);
+}
+
+/**
+ * This method is a generated setter for source0.
+ * @param source the Mat to set
+ */
+void CoolCode::setsource0(cv::Mat &source0){
+	source0.copyTo(this->source0);
+}
+/**
+ * This method is a generated getter for the output of a Resize_Image.
+ * @return Mat output from Resize_Image.
+ */
+cv::Mat* CoolCode::getresizeImageOutput(){
+	return &(this->resizeImageOutput);
+}
+/**
+ * This method is a generated getter for the output of a HSV_Threshold.
+ * @return Mat output from HSV_Threshold.
+ */
+cv::Mat* CoolCode::gethsvThresholdOutput(){
+	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* CoolCode::getfindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+/**
+ * This method is a generated getter for the output of a Filter_Contours.
+ * @return ContoursReport output from Filter_Contours.
+ */
+std::vector<std::vector<cv::Point> >* CoolCode::getfilterContoursOutput(){
+	return &(this->filterContoursOutput);
+}
+	/**
+	 * Scales and image to an exact size.
+	 *
+	 * @param input The image on which to perform the Resize.
+	 * @param width The width of the output in pixels.
+	 * @param height The height of the output in pixels.
+	 * @param interpolation The type of interpolation.
+	 * @param output The image in which to store the output.
+	 */
+	void CoolCode::resizeImage(cv::Mat &input, double width, double height, int interpolation, cv::Mat &output) {
+		cv::resize(input, output, cv::Size(width, height), 0.0, 0.0, interpolation);
+	}
+
+	/**
+	 * Segment an image based on hue, saturation, and value ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param val The min and max value.
+	 * @param output The image in which to store the output.
+	 */
+	void CoolCode::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
+		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
+	}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void CoolCode::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+
+	/**
+	 * Filters through contours.
+	 * @param inputContours is the input vector of contours.
+	 * @param minArea is the minimum area of a contour that will be kept.
+	 * @param minPerimeter is the minimum perimeter of a contour that will be kept.
+	 * @param minWidth minimum width of a contour.
+	 * @param maxWidth maximum width.
+	 * @param minHeight minimum height.
+	 * @param maxHeight  maximimum height.
+	 * @param solidity the minimum and maximum solidity of a contour.
+	 * @param minVertexCount minimum vertex Count of the contours.
+	 * @param maxVertexCount maximum vertex Count.
+	 * @param minRatio minimum ratio of width to height.
+	 * @param maxRatio maximum ratio of width to height.
+	 * @param output vector of filtered contours.
+	 */
+	void CoolCode::filterContours(std::vector<std::vector<cv::Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, std::vector<std::vector<cv::Point> > &output) {
+		std::vector<cv::Point> hull;
+		output.clear();
+		for (std::vector<cv::Point> contour: inputContours) {
+			cv::Rect bb = boundingRect(contour);
+			if (bb.width < minWidth || bb.width > maxWidth) continue;
+			if (bb.height < minHeight || bb.height > maxHeight) continue;
+			double area = cv::contourArea(contour);
+			if (area < minArea) continue;
+			if (arcLength(contour, true) < minPerimeter) continue;
+			cv::convexHull(cv::Mat(contour, true), hull);
+			double solid = 100 * area / cv::contourArea(hull);
+			if (solid < solidity[0] || solid > solidity[1]) continue;
+			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
+			double ratio = bb.width / bb.height;
+			if (ratio < minRatio || ratio > maxRatio) continue;
+			output.push_back(contour);
+		}
+	}
+
+
+
+} // end grip namespace
+

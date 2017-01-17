8367e7c846e6316add4150ddf585a863c3357ddc
Bradley <Bradley21890543@users.noreply.github.com>
2017-01-17 16:25:16 -0500

    :)

--- jetson/grip/gripRunner.py
+++ jetson/grip/gripRunner.py
@@ -1,8 +1,8 @@
 import grip
 import cv2
 camera = cv2.VideoCapture(1)
+cv2.namesWindow("window", cv2.WINDOW_NORMAL)
 while True:
     g = grip.GripPipeline()
     capture = camera.read()
-    print g.process(capture)
-
+    cv2.imshow("window", capture)

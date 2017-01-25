5da9489070638ea68b29e24fb6b3db642394542e
Nick Keirstead <nicholas_keirstead@horacemann.org>
2017-01-25 16:13:49 -0500

    Good Stuff

--- rio/src/org/usfirst/frc/team5806/robot/CameraClient.java
+++ rio/src/org/usfirst/frc/team5806/robot/CameraClient.java
@@ -1,9 +1,10 @@
 import java.net.*;
 import java.io.*;
+import java.awt.image.BufferedImage;
 
 public class CameraClient {
     private String serverName;
-    private INetAddress staticIP;
+    private InetAddress staticIP;
     private int serverPort;
     private Socket client;
     private OutputStream out;
@@ -13,7 +14,7 @@ public class CameraClient {
         this.serverName = serverName;
         this.serverPort = serverPort;
     }
-    public CameraClient(INetAddress staticIP, int serverPort) {
+    public CameraClient(InetAddress staticIP, int serverPort) {
         //Alternative constructor for referencing cam by a static ip rather
         //than a saved name on the LAN.
         this.staticIP = staticIP;
@@ -23,7 +24,7 @@ public class CameraClient {
     public void setServerName(String name) {
         this.serverName = name;
     }
-    public void setStaticIP(INetAddress staticIP) {
+    public void setStaticIP(InetAddress staticIP) {
         this.staticIP = staticIP;
     }
     public void setServerPort(int port) {
@@ -40,7 +41,7 @@ public class CameraClient {
     }
     public void initSocket() {
         try {
-            if (INetAddress == null) {
+            if (staticIP == null) {
                 this.client = new Socket(serverName, serverPort);
             } else {
                 this.client = new Socket(staticIP, serverPort);
@@ -52,16 +53,46 @@ public class CameraClient {
         }
         System.out.println("Client socket successfully initialized.");
     }
-    private byte[] readSocketData() {
-        ByteArrayOutputStream bs = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        for (int s = 0; (s = in.read(buffer)) != -1; ) {
-            bs.write(buffer, 0, s);
+    public void writeString(String s) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+            bw.write(s);
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Error writing string to socket.");
+        }
+    }
+    public String readSocketString() {
+        String s = null;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            s = br.readLine();
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error reading string from socket.");
+        }
+        return s;
+    }
+    private byte[] readSocketBytes() {
+        ByteArrayOutputStream bs = null;
+        try {   
+            bs = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            for (int s = 0; (s = in.read(buffer)) != -1; ) {
+                bs.write(buffer, 0, s);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading bytes from socket.");
         }
         return bs.toByteArray();
     }
-    public BufferedImage readImage(int width, int height) {
+    /*public BufferedImage readImage(int width, int height) {
         byte[] inBytes = readSocketData();
-        //FINISH THIS
+        
+    }*/
+    public static void main(String[] args) {
+        CameraClient driveStation = new CameraClient("", 999);
+        String line = driveStation.readSocketString();
+        System.out.println(line);
     }
 }

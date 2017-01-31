95cf9f9abba15ed9c3042f313a8ffda4891a8833
Michael Truell <michael_truell@horacemann.org>
2017-01-31 16:54:12 -0500

    Start integrating jetson

---
+++ jetson/build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -o output -Wall -std=c++11 -lntcore -pthread -Icomm/include/ -Icomm/wpiutil/include/ -Lcomm/lib -lwpiutil
--- jetson/comm/build.sh
+++
@@ -1,1 +0,0 @@
-g++ main.cpp -o output -Wall -std=c++11 -lntcore -pthread -Iinclude/ -Iwpiutil/include/ -Llib -lwpiutil

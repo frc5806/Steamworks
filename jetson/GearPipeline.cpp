8a57817215b768f973b245bb9f1b41b8b040c1a9
Michael Truell <michael_truell@horacemann.org>
2017-02-03 17:23:57 -0500

    Most of the integration

--- jetson/GearPipeline.cpp
+++ jetson/GearPipeline.cpp
@@ -1,17 +1,17 @@
-#include "Pipeline.h"
+#include "GearPipeline.h"
 
 /**
-* Initializes a Pipeline.
+* Initializes a GearPipeline.
 */
-Pipeline::Pipeline() {
+GearPipeline::GearPipeline() {
 }
 /**
-* Runs an iteration of the Pipeline and updates outputs.
+* Runs an iteration of the GearPipeline and updates outputs.
 *
 * Sources need to be set before calling this method. 
 *
 */
-void Pipeline::Process(){
+void GearPipeline::Process(){
 	//Step Resize_Image0:
 	//input
 	Mat resizeImageInput = source0;
@@ -52,35 +52,35 @@ void Pipeline::Process(){
  * This method is a generated setter for source0.
  * @param source the Mat to set
  */
-void Pipeline::setsource0(Mat &source0){
+void GearPipeline::setsource0(Mat &source0){
 	source0.copyTo(this->source0);
 }
 /**
  * This method is a generated getter for the output of a Resize_Image.
  * @return Mat output from Resize_Image.
  */
-Mat* Pipeline::getresizeImageOutput(){
+Mat* GearPipeline::getresizeImageOutput(){
 	return &(this->resizeImageOutput);
 }
 /**
  * This method is a generated getter for the output of a HSV_Threshold.
  * @return Mat output from HSV_Threshold.
  */
-Mat* Pipeline::gethsvThresholdOutput(){
+Mat* GearPipeline::gethsvThresholdOutput(){
 	return &(this->hsvThresholdOutput);
 }
 /**
  * This method is a generated getter for the output of a Find_Contours.
  * @return ContoursReport output from Find_Contours.
  */
-vector<vector<Point> >* Pipeline::getfindContoursOutput(){
+vector<vector<Point> >* GearPipeline::getfindContoursOutput(){
 	return &(this->findContoursOutput);
 }
 /**
  * This method is a generated getter for the output of a Filter_Contours.
  * @return ContoursReport output from Filter_Contours.
  */
-vector<vector<Point> >* Pipeline::getfilterContoursOutput(){
+vector<vector<Point> >* GearPipeline::getfilterContoursOutput(){
 	return &(this->filterContoursOutput);
 }
 	/**
@@ -92,7 +92,7 @@ vector<vector<Point> >* Pipeline::getfilterContoursOutput(){
 	 * @param interpolation The type of interpolation.
 	 * @param output The image in which to store the output.
 	 */
-	void Pipeline::resizeImage(Mat &input, double width, double height, int interpolation, Mat &output) {
+	void GearPipeline::resizeImage(Mat &input, double width, double height, int interpolation, Mat &output) {
 		resize(input, output, Size(width, height), 0.0, 0.0, interpolation);
 	}
 	/**
@@ -104,7 +104,7 @@ vector<vector<Point> >* Pipeline::getfilterContoursOutput(){
 	 * @param val The min and max value.
 	 * @param output The image in which to store the output.
 	 */
-	void Pipeline::hsvThreshold(Mat &input, double hue[], double sat[], double val[], Mat &out) {
+	void GearPipeline::hsvThreshold(Mat &input, double hue[], double sat[], double val[], Mat &out) {
 		cvtColor(input, out, COLOR_BGR2HSV);
 		inRange(out,Scalar(hue[0], sat[0], val[0]), Scalar(hue[1], sat[1], val[1]), out);
 	}
@@ -115,7 +115,7 @@ vector<vector<Point> >* Pipeline::getfilterContoursOutput(){
 	 * @param externalOnly if only external contours are to be found.
 	 * @param contours vector of contours to put contours in.
 	 */
-	void Pipeline::findContours(Mat &input, bool externalOnly, vector<vector<Point> > &contours) {
+	void GearPipeline::findContours(Mat &input, bool externalOnly, vector<vector<Point> > &contours) {
 		vector<Vec4i> hierarchy;
 		contours.clear();
 		int mode = externalOnly ? RETR_EXTERNAL : RETR_LIST;
@@ -139,7 +139,7 @@ vector<vector<Point> >* Pipeline::getfilterContoursOutput(){
 	 * @param maxRatio maximum ratio of width to height.
 	 * @param output vector of filtered contours.
 	 */
-	void Pipeline::filterContours(vector<vector<Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, vector<vector<Point> > &output) {
+	void GearPipeline::filterContours(vector<vector<Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, vector<vector<Point> > &output) {
 		vector<Point> hull;
 		output.clear();
 		for (vector<Point> contour: inputContours) {
--- jetson/main.cpp
+++ jetson/main.cpp
@@ -2,25 +2,80 @@
 #include <chrono>
 #include <cstdio>
 #include <thread>
-#include "ntcore.h"
-
-#include "networktables/NetworkTable.h"
+#include "Socket.hpp"
+#include "opencv2/opencv.hpp"
+#include "ntcore/include/networktables/NetworkTable.h"
+#include "ntcore/include/ntcore.h"
+#include "GearPipeline.h"
 
 using namespace std;
+using namespace cv;
+
+#define QUALITY 5
+#define PORT1 5440
+#define PORT2 5441
+#define ENDCHAR 'e'
+
+struct Networker {
+    NetworkTable gearTable;
+    NetworkTable boilerTable;
+    Networker() { 
+        NetworkTable::SetClientMode();
+        NetworkTable::SetIPAddress("10.58.06.100\n");
+        NetworkTable::Initialize();
+
+        gearTable = NetworkTable::GetTable("vision");
+    }
+    void putGearCoords(int x1, int y1, int x2, int y2) {
+        gearTable.PutNumber("gearx1", x1);
+        gearTable.PutNumber("geary1", y1);
+        gearTable.PutNumber("gearx2", x2);
+        gearTable.PutNumber("geary2", y2);
+    }
+    void putBoilerTable(int x, int y) {
+        boilerTable.PutNumber("x", x);
+        boilerTable.PutNumber("y", y);
+    }
+};
+
+struct CameraStream {
+    VideoCapture cam;
+    Socket socket;
+    vector<int> config;
+
+    CameraStream(int cameraNumber, int port) {
+        cam.open(cameraNumber);
+        socket.setSocket(port);
+
+        if(!cam.open(0))
+            throw 1;
+
+        config.push_back(CV_IMWRITE_JPEG_QUALITY);
+        config.push_back(QUALITY);
+    }
+
+    void sendFrame() {
+        Mat frame;
+        cam >> frame;
+
+        Mat compressedMat;
+        vector<uchar> buff;
+        imencode(".jpg", frame, buff, config);
+        buff.push_back(ENDCHAR);
+
+        unsigned char * data = &buff[0];
+        socket.send(data, (unsigned int)buff.size()+1);
+    }
+};
 
 int main() {
-  NetworkTable::SetClientMode();
-  NetworkTable::SetIPAddress("10.58.06.100\n");
-  
-  NetworkTable::Initialize();
+    // Connect to the rio
+    Networker networker;
+    GearPipeline gearPipe;
+    CameraStream stream(0, 5806);
 
-  std::this_thread::sleep_for(std::chrono::seconds(5));
-  auto nt = NetworkTable::GetTable("test");
+    // Run vision and streaming code
+    while (true) { 
 
-  while (true) { 
-    int i = rand() % 1000 + 1;
-    std::cout << nt->GetNumber("randomvalue") << "\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-  }
-
+    }
 }
